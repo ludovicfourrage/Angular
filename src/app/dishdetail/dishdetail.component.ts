d436d4eb49ac43cba0978158fdd5cb3b78ecb13a
Ludovic Fourrage <fourrage@hotmail.com>
2017-12-30 11:00:05 -0800

    HTTP Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -22,6 +22,7 @@ export class DishdetailComponent implements OnInit {
 
   commentForm: FormGroup;
   comment: Comment;
+  errMess: string;
 
   formErrors = {
     'rating': '',
@@ -91,7 +92,9 @@ export class DishdetailComponent implements OnInit {
   }
     
   ngOnInit() {
-    this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+    this.dishservice.getDishIds().subscribe(
+      dishIds => this.dishIds = dishIds,
+      errmess => this.errMess = <any>errmess);
     this.route.params
       .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
       .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
